b818655e3add4c6788bc13818a23f803e7f796db
蔡南坤 <cainankun@cainankundeMacBook-Pro.local>
2019-08-22 22:36:57 +0800

    fix: 补齐协议和域名

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,17 @@
 // 去掉query
+const hasProtocol: RegExp = /^http(s)?:\/\//igm;
 export function formatUrl (url: string) {
     if(typeof url === 'string') {
+        // 补齐域名和协议
+        let prefix: string = '';
+        if(!hasProtocol.test(url)) {
+            prefix = location.protocol + '//';
+        }
+        if(url.indexOf(location.host) === -1) {
+            prefix = prefix + location.host + '/';
+        }
+        url = prefix + url;
+
         return url.split('?')[0];
     }else {
         return url;
